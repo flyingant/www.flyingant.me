a85fdd83a518e3f823a492f2e3e3b82f4f504102
FlyingAnt <flying2010.liu@gmail.com>
2019-02-11 11:52:09 +0800

    renew my personal website

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import '../css/main.css';
 import './style.scss';
 
-console.log("Wanna make a new friend? Shoot me message to %c'liucheng@flyingant.me'", 'color: red');
+console.log("Wanna make a new friend or hire me? Shoot the message to %c'liucheng@flyingant.me'", 'color: red');
