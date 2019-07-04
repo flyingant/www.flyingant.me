ab5768cf32f736237ea03794244ab4b515f0c23e
mayi <mayi@wiredcraft.com>
2019-07-04 16:46:36 +0800

    Update the personal site 1. Re-formatted the code and improve the code style 2. Renew the basic view of the site

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import '../css/main.css';
-import './style.scss';
+import '../assets/css/main.css';
+import './styles/style.scss';
 
-console.log("Wanna make a new friend or hire me? Shoot the message to %c'liucheng@flyingant.me'", 'color: red');
+console.log(`
+Wanna make a new friend or hire me? 
+Shoot the message to %c'liucheng@flyingant.me'
+`, 'color: red');
